8da9b7506b966e037133c09546a4577f6b030712
Unknown <m.traninger@aon.at>
2020-11-06 10:12:11 +0100

    Add 2 files to implement unit testing

---
+++ core.py
@@ -0,0 +1,3 @@
+def foo(x)
+	"""Add 1 to x"""
+	return x+1
---
+++ test_core.py
@@ -0,0 +1,6 @@
+from core import foo		"""Hole Funktion Foo aus Package Core"""
+
+def test_foo():			"""Name markiert Funktion als Testfunktion"""
+	assert foo(2) == 3 	"""Testet Funktio und schaut ob Funktion den richtigen Wert ausgibt"""
+
+"""Ausführen des Tests in Python Konsole mit programm pytest"""
